c413ec23f9d7061a58607df97ee7f7eda561fba0
mr-sunny-side <contact@digitalgeek.tech>
2025-12-11 17:57:22 +0900

    12-11

--- mbox_project_remind/ex26_8/ex26_8_6.sh
+++ mbox_project_remind/ex26_8/ex26_8_6.sh
@@ -1,22 +1,32 @@
-"""
+#!/bin/bash
+
+: <<'COMMENT'
 独立した実行scriptとして以下の関数を使用できるように、この.shを記述する
 以下の関数はzshrcと独立して、このファイルが実行された際にのみ定義されること
 その際zshrcに定義されている環境変数を使うので、その辺も考慮
-"""
+COMMENT
+
+# mbox解析プロジェクトのルートディレクトリ
+export MBOX_PROJECT="$HOME/pv/mbox_project_remind"
+
+# プロジェクト内のディレクトリ
+export C_FILE="$MBOX_PROJECT/test_case/out"
+export TXT_FILE="$MBOX_PROJECT/test_case/txt"
+export MBOX="$MBOX_PROJECT/mbox"
 
 ex26_8_ex () {
 
-    local prog=$1
-    local prog_c=$C_FILE/${prog}_file
-    local prog_py=$MBOX_PROJECT/ex26_8/${prog}.py
-    local mbox=$MBOX/google.mbox
-    local output=$MBOX_PROJECT/ex26_8/txt/${prog}_sh.txt
+    local prog="$1"
+    local prog_c="$C_FILE/${prog}_file"
+    local prog_py="$MBOX_PROJECT/ex26_8/${prog}.py"
+    local mbox="$MBOX/google.mbox"
+    local output="$MBOX_PROJECT/ex26_8/txt/${prog}_sh.txt"
 
     echo "========== C result ==========" > $output
     echo "" >> $output
 
     for i in {1..5}; do
-        ($prog_c $mbox 2>&1) | grep "Processing Time" >> $output;
+        ("$prog_c" "$mbox" 2>&1) | grep "Processing Time" >> $output;
         if [ $? -ne 0 ]; then
             echo "✕ Program Failed" >&2
             echo "Executable File: $prog_c" >&2
@@ -29,7 +39,7 @@ ex26_8_ex () {
     echo "" >> $output
 
     for i in {1..5}; do
-        (python3 $prog_py $mbox 2>&1) | grep "Processing Time" >> $output;
+        (python3 "$prog_py" "$mbox" 2>&1) | grep "Processing Time" >> $output;
         if [ $? -ne 0 ]; then
             echo "✕ Program Failed" >&2
             echo "Executable File: $prog_py" >&2
