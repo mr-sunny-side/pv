2782f97a51aa1b2634a3be7f973d7f03308bf6f1
mr-sunny-side <contact@digitalgeek.tech>
2025-12-12 23:16:27 +0900

    12-12

---
+++ mbox_project_remind/ex26_8/ex26_8_7/ex26_8_7.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+#define SEARCH_PREFIX "From: "
+#define PREFIX_LEN 6
+
+int	ext_email_and_copy(char *from_line, char **email) {
+
+	char	*start = NULL;
+	char	*end = NULL;
+
+	if ((start = strchr(from_line, '<')) != NULL) {
+		start++;
+		end = strchr(from_line, '>');
+		if (end == NULL)
+			return -1;
+	} else if ((start = strchr(from_line, ' ') ) != NULL) {
+		start++;
+		end = strchr(from_line, '\n');
+		if (end == NULL)
+			return -1;
+	} else
+		return -1;
+
+	int	interval = end - start;
+	*email = malloc(interval + 1);
+	if (*email == NULL)
+		return 1;
+
+	strncpy(*email, start, interval);
+	//演算子の優先順位
+	(*email)[interval] = '\0';
+	return 0;
+}
+
+int	main(int argc, char **argv) {
+
+	if (argc != 2) {
+		fprintf(stderr, "Argument Error\n");
+		return 1;
+	}
+
+	if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+		// print usage
+		return 0;
+	}
+
+	const char	*file_name = argv[1];
+	FILE		*fp = fopen(file_name, "r");
+	if (fp == NULL) {
+		fprintf(stderr, "Cannot Open File %s\n", file_name);
+		return 1;
+	}
+
+	char	buffer[BUFFER_SIZE];
+	char	*email = NULL;
+	while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+		if (strncmp(buffer, SEARCH_PREFIX, PREFIX_LEN) == 0) {
+
+			int	result = ext_email_and_copy(buffer, &email);
+			if (result == -1)
+				fprintf(stderr, "Cannot Extract Email\n");
+			else if (result == 1) {
+				fprintf(stderr, "Memory Allocation Incomplete\n");
+				free(email);
+				fclose(fp);
+				return 1;
+			}
+			printf("%s\n", email);
+			free(email);
+			email = NULL;
+		}
+
+	}
+
+	fclose(fp);
+	return 0;
+
+}
---
+++ mbox_project_remind/ex26_8/ex26_8_8/ex26_8_8.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+pythonと共有するドメイン抽出関数を書く
+その後、makefileみたいな感じで共有ライブラリとしてコンパイルする
+
+※注意※ free責任は呼び出し側、つまり呼び出すpythonにあるので、free用の関数も書く
+*/
+
+//pythonから呼び出されるから、シンプルに引数の文字列からdomainを抽出し、それを返す関数
+char	*ext_domain(const char *email) {
+	char *here_is_at = NULL;
+	char *end;
+
+	if ((here_is_at = strchr(email, '@')) != NULL)
+		here_is_at++;
+	else
+		return NULL;
+
+
+	int	interval = 0;
+	char	*domain = NULL;
+	if ((end = strchr(email, '>')) != NULL || (end = strchr(email, '\n')) != NULL) {
+		interval = end - here_is_at;
+		domain = malloc(interval + 1);
+		if (domain == NULL)
+			return NULL;
+
+		strncpy(domain, here_is_at, interval);
+		domain[interval] = '\0';
+	}
+
+	return domain;
+}
+
+void	free_memory(char *str) {
+	free(str);
+}
