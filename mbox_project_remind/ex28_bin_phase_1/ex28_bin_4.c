fa740d2f39b1c801931eb5232dd30671c3f5d779
mr-sunny-side <contact@digitalgeek.tech>
2025-12-21 19:55:32 +0900

    12-21

--- mbox_project_remind/ex28_bin_phase_1/ex28_bin_4.c
+++ mbox_project_remind/ex28_bin_phase_1/ex28_bin_4.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
+
+// 12-21: ロジックエラー修正
 
 #pragma pack(push, 1)
 typedef	struct {
@@ -37,38 +40,139 @@ typedef	struct {
 	2. BGR情報を取得し、Pixel構造体に直接格納
 */
 
-int	get_pixel(FILE *fp, const BmpFileHeader *fh, const BmpInfoHeader *ih,
+int	get_pixels(FILE *fp, const BmpFileHeader *fh, const BmpInfoHeader *ih,
 			int x, int y, Pixel *px)
 {
 	// x, yのエラーハンドリング
-	if (x < 0 || x >= ih->width || y < 0 || y >= ih->height) {
-		fprintf(stderr, "get_pixel: Invalid Number\n");
+	if (x < 0 || x >= ih->width || y < 0 || y >= ih->height)
 		return -1;
-	}
 
 	// パディングの計算
-	int bytes_per_pixel = ih->bit_depth / 8;	// ビット深度をbytes単位に変換
-	int row_size = ih->width * bytes_per_pixel;	// 一行当たりのbytesサイズを計算
-	int padding = (4 - (row_size % 4)) % 4;		// 一行当たりのサイズから、4bytes単位でパディングした際のパディング値を算出
-	int padded_row_size = row_size + padding;	// パディング値を含めた一行のサイズを算出
+	int	bytes_per_pixel = ih->bit_depth / 8;	// ビット深度をbytes単位に変換
+	int	row_size = ih->width * bytes_per_pixel;	// 一行当たりのbytesサイズを計算
+	int	padding = (4 - (row_size % 4)) % 4;	// 一行当たりのサイズから、4bytes単位でパディングした際のパディング値を算出
+	int	padded_row_size = row_size + padding;	// パディング値を含めた一行のサイズを算出
 
 	// Y軸を反転
-	int inverted_y = ih->height - 1 - y;		// x, yはidxなので、0始まりになるようにしている(-1)
+	int	inverted_y = ih->height - 1 - y;	// x, yはidxなので、0始まりになるようにしている(-1)
 
 	// 指定の座標のoffsetを算出
-	long offset = fh->pixel_offset +		// pixelデータの先頭バイト位置
+	long	offset = fh->pixel_offset +		// pixelデータの先頭バイト位置
 		      inverted_y * padded_row_size +	// + y座標(反転) * 一行当たりのサイズ(パディング込み) == 先頭からの行バイト距離
 		      x * bytes_per_pixel;		// + x座標 * 1ピクセル当たりのバイト値 		    == 該当行の先頭からのバイト距離
 
 	// 該当ファイルアドレスに移動
-	if (fseek(fp, offset, SEEK_SET) != 0) {
-		fprintf(stderr, "fseek returned error\n");
+	if (fseek(fp, offset, SEEK_SET) != 0)
+		return 1;
+
+
+	// Pixel構造体にBGRデータをバッファ
+	if (fread(px, sizeof(px), 1, fp) != 1)
+		return 1;
+
+	return 0;
+}
+
+int	print_all_pixels(FILE *fp, const BmpFileHeader *fh, const BmpInfoHeader *ih) {
+	// get_pixels関数をループで呼び出す
+
+	Pixel	px;
+
+	printf("=== All Pixels ===\n");
+	printf("\n");
+
+	int	result = 0;
+
+	// 座標はidxなので-1して整合性を取る
+	// 座標の全探索は基本的に2重ループになるっぽい
+	for (int y = (ih->height - 1); y >= 0; y--) {	// この関数は構造体のポインタしか持ってないので、記述はポインタになる
+		for (int x = 0; x < ih->width; x++) {	// xは反転していないので0でok
+			if ((result = get_pixels(fp, fh, ih, x, y, &px)) != 0)
+				return result;
+			printf("[%d, %d, %d] ", px.blue, px.green, px.red);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+int	main(int argc, char **argv) {
+	if (argc < 3) {
+		fprintf(stderr, "Argument error\n");
+		fprintf(stderr, "Usage: [this file] [.bmp] [X coordinate] [Y coordinate]\n");
+		fprintf(stderr, "Option: [this file] [.bmp] [-a or --all] to show all pixels\n");
+		return 0;
+	}
+
+	const char	*file_name = argv[1];
+	FILE		*fp = fopen(file_name, "rb");
+	BmpFileHeader	fh;
+	BmpInfoHeader	ih;
+
+
+	if (fp == NULL) {
+		fprintf(stderr, "main fopen: returned error\n");
 		return 1;
 	}
 
-	if (fread(px, sizeof(px), 1, fp) != 1) {
-		fprintf(stderr, )
+	if (fread(&fh, sizeof(fh), 1, fp) != 1) {
+		fprintf(stderr, "main fread: returned error\n");
+		fclose(fp);
+		return 1;
 	}
 
 
+	if (fh.file_type != 0x4d42) {	//
+		fprintf(stderr, "File type is not .bmp");
+		fclose(fp);
+		return -1;
+	}
+
+	if (fread(&ih, sizeof(ih), 1, fp) != 1) {
+		fprintf(stderr, "main fread: returned error\n");
+		fclose(fp);
+		return 1;
+	}
+
+	int	result = 0;
+	if (strcmp(argv[2], "-a") == 0 || strcmp(argv[2], "--all") == 0) {
+		result = print_all_pixels(fp, &fh, &ih);
+		if (result != 0) {
+			fprintf(stderr, "print_all_pixels: returned error\n");
+			fclose(fp);
+			return result;
+		}
+		return 0;
+	}
+
+	int	x = atoi(argv[3]);
+	int	y = atoi(argv[4]);
+	Pixel	px;
+	result = get_pixels(fp, &fh, &ih, x, y, &px);
+	if (result != 0) {
+		fprintf(stderr, "get_pixels: returned error\n");
+		return result;
+	}
+
+	/*
+	$ ./pixel_access sample.bmp 0 0
+	Pixel at (0, 0):
+	  Red:   0 (0x00)
+	  Green: 0 (0x00)
+	  Blue:  0 (0x00)
+
+	$ ./pixel_access sample.bmp 7 7
+	Pixel at (7, 7):
+	  Red:   255 (0xFF)
+	  Green: 255 (0xFF)
+	  Blue:  255 (0xFF)
+	*/
+
+	printf("Pixel at (%d, %d):", x, y);
+	printf("	Red: %3d (%02x)", px.red, px.red);
+	printf("	Green: %3d (%02x)", px.green, px.green);
+	printf("	Blue: %3d (%02x)", px.blue, px.blue);
+
+	return 0;
 }
