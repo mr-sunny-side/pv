03745aa9f1d33a4103d809748787d8b15894a66e
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-01-04 00:42:12 +0900

    01-03

--- mbox_project_remind/ex28_bin_phase_2/11d_ex28_bin.c
+++ mbox_project_remind/ex28_bin_phase_2/11d_ex28_bin.c
@@ -3,7 +3,9 @@
 #include <string.h>
 
 // **目的**: 最大振幅、ゼロクロスを1回の走査で計算する(余裕があればRMS)
-// 01-03: process_read関数の作成から
+// 01-03: チャンク情報読み取りまで記述
+//		- 最大振幅、ゼロクロスの操作記述から
+//		- 16bitPCMでない場合、チャンク情報、durationなどを出力して終了する
 
 #pragma pack(push, 1)
 
@@ -29,7 +31,7 @@ typedef	struct {
 	uint16_t	bit_depth;
 }	FmtChunk;
 
-int	process_read(FILE *fp, FmtChunk *fmt, TmpHeader *tmp,int *is_fmt, uint32_t *data_size, long *data_offset) {
+int	process_read(FILE *fp, FmtChunk *fmt, TmpHeader *tmp, int *is_fmt, uint32_t *data_size, long *data_offset) {
 
 	if (fread(tmp, sizeof(TmpHeader), 1, fp) != 1) {
 		fprintf(stderr, "ERROR fread/process_read: Cannot read TmpHeader\n");
@@ -37,6 +39,53 @@ int	process_read(FILE *fp, FmtChunk *fmt, TmpHeader *tmp,int *is_fmt, uint32_t *
 	}
 
 
+	// fmt chunk読み込み
+	if (memcmp(tmp->chunk_id, "fmt ", 4) == 0) {
+		fprintf(stderr, "process_read: fmt chunk detected\n");
+		// fmt chunk先頭へ移動
+		if (fseek(fp, -sizeof(TmpHeader), SEEK_CUR) != 0) {
+			fprintf(stderr, "ERROR fseek/process_read: Cannot move to start of fmt chunk\n");
+			return -1;
+		}
+
+		// fmt chunkを読み込み
+		if (fread(fmt, sizeof(FmtChunk), 1, fp) != 1) {
+			fprintf(stderr, "ERROR fread/process_read: Cannot read fmt chunk\n");
+			return -1;
+		}
+		*is_fmt = 1;
+
+		// 16byte以上のfmt chunkの残りをスキップ
+		uint32_t	fmt_size = (uint32_t)sizeof(FmtChunk) - 8;
+		if (fmt_size < fmt->chunk_size) {
+			uint32_t	skip_size = fmt->chunk_size - fmt_size;
+			if (fseek(fp, skip_size, SEEK_CUR) != 0) {
+				fprintf(stderr, "ERROR fseek/process_read: Cannot skip rest of fmt chunk\n");
+				return -1;
+			}
+			fprintf(stderr, "process_read: Rest of fmt chunk is skipped\n");
+			fprintf(stderr, " - fmt chunk size: %u\n", fmt->chunk_size);
+			fprintf(stderr, " - skip_size: %u\n", skip_size);
+		}
+		fprintf(stderr, "process_read: fmt chunk lorded\n");
+	// data header読み込み
+	} else if (memcmp(tmp->chunk_id, "data", 4) == 0) {
+		fprintf(stderr, "process_read: data header detected\n");
+
+		*data_size = tmp->chunk_size;
+		*data_offset = ftell(fp);
+		fprintf(stderr, " - data_size: %u\n", *data_size);
+		fprintf(stderr, " - data_offset: %ld\n", *data_offset);
+	// 未定義チャンクをスキップ
+	} else {
+		if (fseek(fp, tmp->chunk_size, SEEK_CUR) != 0) {
+			fprintf(stderr, "ERROR fseek/processs_read: Cannot skip unknown chunk\n");
+			return -1;
+		}
+		fprintf(stderr, "process_read: Unknown chunk is skipped [%.4s]\n", tmp->chunk_id);
+	}
+
+	return 0;
 }
 
 int	main(int argc, char **argv) {
@@ -53,17 +102,47 @@ int	main(int argc, char **argv) {
 		return -1;
 	}
 
-	RiffHeader	riff;
+	RiffHeader	riff = {0};
 	if (fread(&riff, sizeof(RiffHeader), 1, fp) != 1) {
 		fprintf(stderr, "ERROR fread/main: Cannot read RIFF header\n");
-		return -1;
+		goto clean_error;
 	}
 
 	if (memcmp(riff.chunk_id, "RIFF", 4) != 0 ||
 		memcmp(riff.format, "WAVE", 4) != 0) {
 		fprintf(stderr, "ERROR main: This file is not WAV\n");
-		return -1;
+		goto clean_error;
+	}
+	fprintf(stderr, "main: This file is WAV\n");
+
+	// fmt, data chunkの読み込みループ
+	TmpHeader	tmp = {0};
+	FmtChunk	fmt = {0};
+	int			is_fmt = 0;
+	uint32_t	data_size = 0;
+	long		data_offset = 0;
+	while (!is_fmt || !data_size || !data_offset) {
+		if (feof(fp))
+			break;
+		if (process_read(fp, &fmt, &tmp, &is_fmt, &data_size, &data_offset) == -1) {
+			fprintf(stderr, "ERROR process_read/main: returned error\n");
+			goto clean_error;
+		}
 	}
 
+	if (!is_fmt || !data_size || !data_offset) {
+		fprintf(stderr, "ERROR main: Cannot find fmt chunk or data chunk\n");
+		fprintf(stderr, "is_fmt: %d\n", is_fmt);
+		fprintf(stderr, "data_size: %u\n", data_size);
+		fprintf(stderr, "data_offset: %ld\n", data_offset);
+		goto clean_error;
+	}
+
+	fclose(fp);
+	return 0;
+
+	clean_error:
+		fclose(fp);
+		return -1;
 
 }
