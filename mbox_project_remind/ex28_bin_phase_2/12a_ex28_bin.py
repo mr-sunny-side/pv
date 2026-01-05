07d00a0c193a2363a57819789dbf5924be177265
mr-sunny-side <197456592+mr-sunny-side@users.noreply.github.com>
2026-01-05 12:15:01 +0900

    01-05

--- mbox_project_remind/ex28_bin_phase_2/12a_ex28_bin.py
+++ mbox_project_remind/ex28_bin_phase_2/12a_ex28_bin.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import struct
 import sys
 
@@ -5,25 +7,113 @@ import sys
 **目的**　統計情報、最大振幅、ゼロクロス回数を表示する
             - 16bit stereo PCM以外は統計情報のみ出力
 
-01-04:  riffチャンクの確認まで記述
-        process_read関数とループの記述から
-        
+01-04:  process_read関数まで記述
+         - main関数内のるーぷ記述から
+         - ループについてはノーションを確認のこと
 
 """
 
+def process_read(f): # bool, fmt_chunk, data_size, data_offset
+    data = f.read(8)
+    if len(data) != 8:
+        print('ERROR read/process_read: Cannot read file', file=sys.stderr)
+        return False
+    tmp_id, tmp_size = struct.unpack('<4sI', data)
+    
+    # fmt chunkの検証
+    if tmp_id == b'fmt ':
+        print('process_read: fmt chunk detected', file=sys.stderr)
+        f.seek(-8, 1)   # fmt chunkの戦闘へ移動
+        
+        # fmt chunkの読み込み
+        data = f.read(24)
+        if len(data) != 24:
+            print('ERROR read/process_read: Cannot read fmt chunk', file=sys.stderr)
+            return False
+        
+        chunk_id, chunk_size, audio_format, channel_num, \
+            sample_rate, byte_rate, block_align, bit_depth = struct.unpack('<4sIHHIIHH', data)
+            
+        # fmt chunkが16byte以上の場合、スキップ
+        if 16 < chunk_size:
+            skip_size = chunk_size - 16
+            f.seek(skip_size, 1)
+            print('process_read: Rest of fmt chunk is skipped', file=sys.stderr)
+    
+        # fmt chunk辞書に書き込み
+        print('process_read: fmt chunk is lorded', file=sys.stderr)
+        fmt_chunk = {
+            'chunk_id': chunk_id,
+            'chunk_size': chunk_size,
+            'audio_format': audio_format,
+            'channel_num': channel_num,
+            'sample_rate': sample_rate,
+            'byte_rate': byte_rate,
+            'block_align': block_align,
+            'bit_depth': bit_depth
+        }
+        
+        return True, fmt_chunk, None, None
+    # data chunkの検証
+    elif tmp_id == b'data':
+        print('process_read: data chunk detected', file=sys.stderr)
+        data_size = tmp_size
+        data_offset = f.tell()
+        print(f' - data_size: {data_size}', file=sys.stderr)
+        print(f' - data_offset: {data_offset}', file=sys.stderr)
+        # data chunkは必然的に最後のチャンクなので、終端までスキッ プの必要はない
+        
+        return True, None, data_size, data_offset
+    else:
+        f.seek(tmp_size, 1)
+        print(f'process_read: Unknown chunk is skipped: [{tmp_id.decode()}]', file=sys.stderr)
+        
+        return True, None, None, None
+
+
 def conf_riff(f):
     data = f.read(12)
-    if len(data) !~ 12:
+    if len(data) != 12:
         print('ERROR conf_riff: Cannot read file', file=sys.stderr)
-        return -1
+        return False
             
     chunk_id, chunk_size, data_format = struct.unpack('<4sI4s', data)
+    
     if chunk_id != b'RIFF' or data_format != b'WAVE':
         print('ERROR conf_riff: This file is not WAV', file=sys.stderr)
-        return -1
+        return False
+        
+    return True
 
-def process_read(data):
+def print_stat(fmt_chunk, data_size):
+    """
+    === WAV File Information ===
+    Audio format: 1 (PCM)
+    Channels: 1 (Mono)
+    Sample rate: 44100 Hz
+    Bits per sample: 16
+    Data size: 88200 bytes
+    Duration: 1.00 seconds
     
+    """
+    
+    audio_format = fmt_chunk['audio_format']
+    is_pcm = 'PCM' if audio_format == 1 else 'Unknown'
+    
+    channels = fmt_chunk['channel_num']
+    is_mono = 'Mono' if channels == 1 else 'Stereo'
+    
+    sample_rate = fmt_chunk['sample_rate']
+    bits_per_sample = fmt_chunk['bit_depth']
+    duration = data_size / fmt_chunk['byte_rate']
+    
+    print('=== WAV File Information ===')
+    print(f'Audio format: {audio_format} ({is_pcm})')
+    print(f'Channels: {channels} ({is_mono})')
+    print(f'Sample rate: {sample_rate} Hz')
+    print(f'Bit per sample: {bits_per_sample}')
+    print(f'Data size: {data_size}')
+    print(f'Duration: {duration:.2f}')
 
 def main():
     if len(sys.argv) != 2:
@@ -34,15 +124,65 @@ def main():
     try:
         with open(file_name, "rb") as f:
             # waveフォーマットか確認
-            if conf_riff(f) == -1
+            if not conf_riff(f):
                 print('ERROR conf_riff/main: returned error', file=sys.stderr)
                 
             # fmt, dataチャンクの取得
+            process_bool = False
+            fmt_chunk = {}
+            data_size = 0
+            data_offset = 0
+            while not fmt_chunk or not data_size or not data_offset:
+                # process_readがエラーを返したら、一旦ループをやめる
+                process_bool, fmt_chunk, data_size, data_offset = \
+                    process_read(f)
+                if process_bool == False:
+                    break
+            
+            # 各チャンクが読み込まれているか検証
+            if not fmt_chunk or not data_size or not data_offset:
+                print('ERROR main: Cannot find fmt of data chunk', file=sys.stderr)
+                return -1
+            
+            #16bitPCM stereoか検証
+            #違ったら統計情報をそのまま出力
+            if fmt_chunk['audio_format'] != 1 or fmt_chunk['bit_depth'] != 16 or \
+                fmt_chunk['channel_num'] != 2:
+                print('main: This file is not 16bitPCM Stereo WAV', file =sys.stderr)
+                print('', file=sys.stderr)
+                
+                print_stat(fmt_chunk, data_size)
+                
+            return 0
+                
+                
             
     # 正しい記述を忘れた
-    except FIleNotExistError as e:
+    except FileNotExistError as e:
         print(e)
         return -1
-    except Exception ae e:
+    except Exception as e:
         print(e)
         return -1
+        
+if __name__ == '__main__':
+    sys.exit(main())
+    
+    
+"""
+以下は12aの出力です。まだ書いている途中ですが、一旦ここまでをテストしています。何が間違っているか教えてください。
+
+```bash
+➜  ex28_bin_phase_2 git:(main) ✗ python3 12a_ex28_bin.py $BIN_FILE/sample.wav
+process_read: fmt chunk detected
+process_read: fmt chunk is lorded
+process_read: data chunk detected
+ - data_size: 88200
+ - data_offset: 44
+process_read: Unknown chunk is skipped: []
+ERROR read/process_read: Cannot read file
+ERROR main: Cannot find fmt of data chunk
+```
+
+
+"""
